0d3f08673d9517486e7ef19d231f7477d0b34673
ArseniyMuravyev <aamuravyev@icloud.com>
2024-07-01 15:17:04 +0300

    vercel.json added

--- apps/frontend/src/utils/api.ts
+++ apps/frontend/src/utils/api.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const api = axios.create({
-	withCredentials: true,
-	baseURL: "http://localhost:5000/",
-	headers: {
-		"Content-Type": "application/json",
-	},
+  withCredentials: true,
+  baseURL: "http://localhost:5001/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
-	config.headers.Authorization = `Bearer ${localStorage.getItem(
-		"accessToken"
-	)}`;
-	return config;
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
+  return config;
 });
